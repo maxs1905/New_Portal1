10bc1ab72732c1d29c7f64c983d3c09e22d20910
Maxs <maxs05112001@mail.ru>
2025-01-22 15:08:58 +0400

    New_portalv12

--- News_Portal/news_portal_mod/apps.py
+++ News_Portal/news_portal_mod/apps.py
@@ -3,7 +3,7 @@ class NewsPortalModConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'news_portal_mod'
     def ready(self):
-        import News_Portal.news_portal_mod.signals
+        import news_portal_mod.signals
 
 
 
